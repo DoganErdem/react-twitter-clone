396776c4abd8d7ec4d5772f04abfedf58886a0b4
Volkan Günay <devdarfy@gmail.com>
2022-12-27 19:55:27 +0300

    General fixes

--- src/App.js
+++ src/App.js
@@ -24,150 +24,206 @@ function App() {
     setId(id + 1);
   };
 
+  const Search = () => {
+    return (
+      <div className="search">
+        <div className="search-icon">
+          <BiSearch size={22} />
+        </div>
+        <div className="search-text">
+          <input
+            className="search-text-tweet"
+            type={"text"}
+            placeholder="Search Twitter"
+          ></input>
+        </div>
+      </div>
+    );
+  };
+
+  const FollowAdvices = () => {
+    return (
+      <div className="who">
+        <div className="who-text">
+          <span>Who to follow</span>
+        </div>
+        <a href="">
+          <div className="who-follow">
+            <div className="who-follow-name">
+              <BsPersonCircle size={30} color="white" />
+              <div className="who-follow-name-name">
+                <div className="who1">
+                  <div className="who-name">Volkan Günay</div>
+                  <div className="who-mail">@volkangünay</div>
+                </div>
+                <div className="who-follow-follow">
+                  <span>Follow</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div className="who-follow">
+            <div className="who-follow-name">
+              <BsPersonCircle size={30} color="white" />
+              <div className="who-follow-name-name">
+                <div className="who1">
+                  <div className="who-name">Galatasaray</div>
+                  <div className="who-mail">@galatasaray</div>
+                </div>
+                <div className="who-follow-follow">
+                  <span>Follow</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="" className="who-show">
+          <div className="who-show-more">
+            <span>Show more</span>
+          </div>
+        </a>
+      </div>
+    );
+  };
+
+  const Trends = () => {
+    return (
+      <div className="trend">
+        <div className="trend-text">
+          <span>Trends for you</span>
+        </div>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+        <a href="">
+          <div className="trend-box">
+            <div className="trend-box-top">
+              <span>Trending in Turkey</span>
+              <RiMoreFill className="icon-more" />
+            </div>
+            <div>Dünya Kupası</div>
+            <div className="trend-box-bottom">54k tweet</div>
+          </div>
+        </a>
+      </div>
+    );
+  };
+
   const RightSide = () => {
     return (
       <div className="rigthside">
-        <div className="search">
-          <div className="search-icon">
-            <BiSearch size={22} />
-          </div>
-          <div className="search-text">
-            <input
-              className="search-text-tweet"
-              type={"text"}
-              placeholder="Search Twitter"
-            ></input>
-          </div>
-        </div>
-        <div className="trend">
-          <div className="trend-text">
-            <span>Trends for you</span>
-          </div>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-          <a href="">
-            <div className="trend-box">
-              <div className="trend-box-top">
-                <span>Trending in Turkey</span>
-                <RiMoreFill className="icon-more" />
-              </div>
-              <div>Dünya Kupası</div>
-              <div className="trend-box-bottom">54k tweet</div>
-            </div>
-          </a>
-        </div>
-
-        <div className="who">
-          <div className="who-text">
-            <span>Who to follow</span>
-          </div>
-          <a href="">
-            <div className="who-follow">
-              <div className="who-follow-name">
-                <BsPersonCircle size={30} color="white" />
-                <div className="who-follow-name-name">
-                  <div className="who1">
-                    <div className="who-name">Volkan Günay</div>
-                    <div className="who-mail">@volkangünay</div>
-                  </div>
-                  <div className="who-follow-follow">
-                    <span>Follow</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-          <a href="">
-            <div className="who-follow">
-              <div className="who-follow-name">
-                <BsPersonCircle size={30} color="white" />
-                <div className="who-follow-name-name">
-                  <div className="who1">
-                    <div className="who-name">Galatasaray</div>
-                    <div className="who-mail">@galatasaray</div>
-                  </div>
-                  <div className="who-follow-follow">
-                    <span>Follow</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-          <a href="" className="who-show">
-            <div className="who-show-more">
-              <span>Show more</span>
-            </div>
-          </a>
-        </div>
+        <Search />
+        <Trends />
+        <FollowAdvices />
       </div>
+    );
+  };
+
+  const Tweet = (text) => {
+    return (
+      <li>
+        <div className="tweet-up">
+          <div className="tweet-up-profile">
+            <BsPersonCircle size={45} />
+          </div>
+          <div className="tweet-up-article">
+            <div className="tweet-up-article-name">
+              <span>Doğan Erdem Kara </span>
+              <span> @doganerdem</span>
+              <a href="">
+                <RiMoreFill className="icon-more" />
+              </a>
+            </div>
+            <span>{text.text}</span>
+            <div className="tweet-down">
+              <a href="" className="tweet-down-icon">
+                <FaRegComment size={15} color="darkcyan" />
+                <span>183</span>
+              </a>
+              <a href="">
+                <FaRetweet size={19} color="darkcyan" />
+                <span>283</span>
+              </a>
+              <a href="">
+                <FaRegHeart size={15} color="darkcyan" />
+                <span>193</span>
+              </a>
+              <a href="">
+                <FiShare size={17} color="darkcyan" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </li>
     );
   };
 
@@ -250,40 +306,7 @@ function App() {
         <div className="tweet">
           <ul>
             {tweet.map(({ text, id }) => (
-              <li key={id}>
-                <div className="tweet-up">
-                  <div className="tweet-up-profile">
-                    <BsPersonCircle size={45} />
-                  </div>
-                  <div className="tweet-up-article">
-                    <div className="tweet-up-article-name">
-                      <span>Doğan Erdem Kara </span>
-                      <span> @doganerdem</span>
-                      <a href="">
-                        <RiMoreFill className="icon-more" />
-                      </a>
-                    </div>
-                    <span>{text}</span>
-                    <div className="tweet-down">
-                      <a href="" className="tweet-down-icon">
-                        <FaRegComment size={15} color="darkcyan" />
-                        <span>183</span>
-                      </a>
-                      <a href="">
-                        <FaRetweet size={19} color="darkcyan" />
-                        <span>283</span>
-                      </a>
-                      <a href="">
-                        <FaRegHeart size={15} color="darkcyan" />
-                        <span>193</span>
-                      </a>
-                      <a href="">
-                        <FiShare size={17} color="darkcyan" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </li>
+              <Tweet key={id} text={text} />
             ))}
           </ul>
         </div>
